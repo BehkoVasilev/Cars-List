9b1b9bcafaa9198133b8f4a306196900e6821f30
BehkoVasilev <vencislavvsilev@gmail.com>
2023-05-16 12:56:54 +0300

    Update carSlice.js
    generate ID with nanoid

---
+++ src/store/slices/carSlice.js
@@ -0,0 +1,34 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const carsSlice = createSlice({
+    name: 'cars',
+    initialState: {
+        searchTerm: '',
+        cars: []
+    },
+    reducers: {
+        changeSearchTerm(state, action) {
+            state.searchTerm = action.payload;
+        },
+        addCar(state, action) {
+            /* Assumbtion:
+            action.payload === { name: 'asd', cost: 200 } */
+            state.cars.push({
+                name: action.payload.name,
+                cost: action.payload.cost,
+                id: nanoid()
+            });
+        },
+        removeCar(state, action) {
+            /* Assumbtion:
+            action.payload === the id of the car we want to remove */
+            state.cars = state.cars.filter((car) => car.id !== action.payload);
+        },
+    },
+});
+
+export const {
+    changeSearchTerm, addCar, removeCar
+} = carsSlice.actions;
+
+export const carsReducer = carsSlice.reducer;
