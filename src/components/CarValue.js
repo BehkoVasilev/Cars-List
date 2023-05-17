89b29701297f01eaee3ecb42377aec72f69d870b
BehkoVasilev <vencislavvsilev@gmail.com>
2023-05-17 11:40:54 +0300

    display the total cost of cars in the list

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,5 +1,27 @@
+import { useSelector } from 'react-redux';
+
 function CarValue() {
-    return <div>CarValue</div>
+    const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+        return data.filter(car =>
+            car.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+            .reduce((acc, car) => acc + car.cost, 0)
+
+        // const filteredCars = data.filter((car) => {
+        //     return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+        // });
+        // // let cost = 0
+        // // filteredCars.map(car => cost += car.cost);
+        // // return cost
+
+        // return filteredCars.reduce((acc, car) => {
+        //     return acc + car.cost
+        // }, 0)
+    })
+
+    return <div className="car-value">
+        Total Cost: ${totalCost}
+    </div>
 };
 
 export default CarValue;
