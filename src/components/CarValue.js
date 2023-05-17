c01ec9f9f6e04116d7f0b205e323a884f74ad866
BehkoVasilev <vencislavvsilev@gmail.com>
2023-05-17 11:43:12 +0300

    the simplest way of counting total cost

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,10 +1,11 @@
 import { useSelector } from 'react-redux';
 
 function CarValue() {
-    const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
-        return data.filter(car =>
-            car.name.toLowerCase().includes(searchTerm.toLowerCase())
-        )
+    const totalCost = useSelector(({ cars: { data, searchTerm } }) =>
+        data
+            .filter(car =>
+                car.name.toLowerCase().includes(searchTerm.toLowerCase())
+            )
             .reduce((acc, car) => acc + car.cost, 0)
 
         // const filteredCars = data.filter((car) => {
@@ -17,7 +18,7 @@ function CarValue() {
         // return filteredCars.reduce((acc, car) => {
         //     return acc + car.cost
         // }, 0)
-    })
+    )
 
     return <div className="car-value">
         Total Cost: ${totalCost}
