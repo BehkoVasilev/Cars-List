0f627f7bd9a6a23e9e14c4b9941fdd2487a51044
BehkoVasilev <vencislavvsilev@gmail.com>
2023-05-17 12:01:39 +0300

    Highlighting existing cars

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -7,18 +7,7 @@ function CarValue() {
                 car.name.toLowerCase().includes(searchTerm.toLowerCase())
             )
             .reduce((acc, car) => acc + car.cost, 0)
-
-        // const filteredCars = data.filter((car) => {
-        //     return car.name.toLowerCase().includes(searchTerm.toLowerCase());
-        // });
-        // // let cost = 0
-        // // filteredCars.map(car => cost += car.cost);
-        // // return cost
-
-        // return filteredCars.reduce((acc, car) => {
-        //     return acc + car.cost
-        // }, 0)
-    )
+    );
 
     return <div className="car-value">
         Total Cost: ${totalCost}
